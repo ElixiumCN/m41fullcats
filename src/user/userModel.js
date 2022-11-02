866c1210d195b453566453a8fa5cfa409d0ab8b5
ElixiumCN <100935972+ElixiumCN@users.noreply.github.com>
2022-11-02 17:03:29 +0000

    .
    .

--- src/user/userModel.js
+++ src/user/userModel.js
@@ -1,38 +1,34 @@
-const mongoose = require("mongoose");
-const jwt = require("jsonwebtoken");
-const bcrypt = require("bcryptjs");
+const mongoose = require('mongoose')
+const bcrypt = require('bcrypt')
 
 const userSchema = new mongoose.Schema({
-  username: {
-    type: String,
-    required: true,
-    unique: true,
-  },
-  email: {
-    type: String,
-    required: true,
-    unique: true,
-    match: /.+\@.+\..+/,
-  },
-  password: {
-    type: String,
-    required: true,
-  },
-});
+    username: {
+        type: String,
+        required: true,
+        unique: true
+    },
+    email: {
+        type: String,
+        required: true,
+        unique: true,
+        match: /.+\@.+\..+/,
+    },
+    password : {
+        type: String,
+        required: true
+    }
+})
 
-userSchema.methods.generateAuthToken = function () {
-  console.log(this);
-  return jwt.sign({ _id: this._id }, process.env.SECRET);
-};
+userSchema.statics.findByCredentials = async (username, password) => {
+    const user = await User.findOne({username})
+    console.log(user)
+    if (user && await bcrypt.compare(password, user.password)) {
+        return user
+    } else {
+        throw new Error()
+    }
+}
 
-userSchema.statics.findByCredentials = async (username, pass) => {
-  const user = await User.findOne({ username });
-  if (user && bcrypt.compare(pass, user.password)) {
-    return user;
-  }
-  throw new Error();
-};
+const User = mongoose.model('user', userSchema)
 
-const User = mongoose.model("User", userSchema);
-
-module.exports = User;
+module.exports = User
